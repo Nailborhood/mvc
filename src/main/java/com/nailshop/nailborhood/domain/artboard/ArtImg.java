78df638a40544ed56ebba350370a053d466d8df4
younssue <love_rvd7@naver.com>
2024-02-01 17:38:59 +0900

    :art: domain set up complete

---
+++ src/main/java/com/nailshop/nailborhood/domain/artboard/ArtImg.java
@@ -0,0 +1,29 @@
+package com.nailshop.nailborhood.domain.artboard;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ArtImg {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    private String imgPath;
+    private int imgNum;
+    private Boolean isDeleted;
+
+    @Builder
+    public ArtImg(String imgPath, int imgNum, Boolean isDeleted) {
+        this.imgPath = imgPath;
+        this.imgNum = imgNum;
+        this.isDeleted = isDeleted;
+    }
+}
---
+++ src/main/java/com/nailshop/nailborhood/domain/common/BaseTime.java
@@ -0,0 +1,22 @@
+package com.nailshop.nailborhood.domain.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    private LocalDateTime createdAt; // 셍성 시간
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt; // 수정 시간
+}
