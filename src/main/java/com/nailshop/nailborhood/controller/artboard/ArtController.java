631f19c67930cfd3519926e9ce9ddb4ffb593ae6
wnsdy <wnsdyd1012@gmail.com>
2024-02-04 19:06:07 +0900

    :art: [#17] develop Artboard modification

--- src/main/java/com/nailshop/nailborhood/controller/artboard/ArtController.java
+++ src/main/java/com/nailshop/nailborhood/controller/artboard/ArtController.java
@@ -1,17 +1,16 @@
 package com.nailshop.nailborhood.controller.artboard;
 
 import com.nailshop.nailborhood.dto.artboard.ArtRegistrationDto;
+import com.nailshop.nailborhood.dto.artboard.ArtUpdateDto;
 import com.nailshop.nailborhood.dto.common.CommonResponseDto;
 import com.nailshop.nailborhood.dto.common.ResultDto;
 import com.nailshop.nailborhood.service.artboard.ArtRegistrationService;
+import com.nailshop.nailborhood.service.artboard.ArtUpdateService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -22,13 +21,26 @@ import java.util.List;
 public class ArtController {
 
     private final ArtRegistrationService artRegistrationService;
+    private final ArtUpdateService artUpdateService;
 
     @Tag(name = "owner", description = "owner API")
     @Operation(summary = "아트판 등록", description = "owner API")
     @PostMapping(consumes = {"multipart/form-data"}, value = "/owner/artboard/resister")
     public ResponseEntity<ResultDto<Void>> registerArt(@RequestPart(value = "file") List<MultipartFile> multipartFileList,
-                                                       @RequestPart(value = "data")ArtRegistrationDto artRegistrationDto){
+                                                       @RequestPart(value = "data") ArtRegistrationDto artRegistrationDto){
         CommonResponseDto<Object> commonResponseDto = artRegistrationService.registerArt(multipartFileList, artRegistrationDto);
+        ResultDto<Void> resultDto = ResultDto.in(commonResponseDto.getStatus(), commonResponseDto.getMessage());
+
+        return ResponseEntity.status(commonResponseDto.getHttpStatus()).body(resultDto);
+    }
+
+    @Tag(name = "owner", description = "owner API")
+    @Operation(summary = "아트판 수정", description = "owner API")
+    @PutMapping(consumes = {"multipart/form-data"}, value = "/owner/artboard/modify/{artRefId}")
+    public ResponseEntity<ResultDto<Void>> updateArtRef(@PathVariable Long artRefId,
+                                                        @RequestPart(value = "file") List<MultipartFile> multipartFileList,
+                                                        @RequestPart(value = "data") ArtUpdateDto artUpdateDto){
+        CommonResponseDto<Object> commonResponseDto = artUpdateService.updateArt(multipartFileList, artUpdateDto, artRefId);
         ResultDto<Void> resultDto = ResultDto.in(commonResponseDto.getStatus(), commonResponseDto.getMessage());
 
         return ResponseEntity.status(commonResponseDto.getHttpStatus()).body(resultDto);
---
+++ src/main/java/com/nailshop/nailborhood/dto/artboard/ArtUpdateDto.java
@@ -0,0 +1,12 @@
+package com.nailshop.nailborhood.dto.artboard;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ArtUpdateDto {
+
+    private String name;
+    private String content;
+}
