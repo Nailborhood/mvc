f6609a85d0da621c714c1b3ca0f472c6337fed3f
wnsdy <wnsdyd1012@gmail.com>
2024-02-04 16:57:16 +0900

    :art: [#17] develop Artboard Registration

---
+++ src/main/java/com/nailshop/nailborhood/repository/artboard/ArtImgRepository.java
@@ -0,0 +1,7 @@
+package com.nailshop.nailborhood.repository.artboard;
+
+import com.nailshop.nailborhood.domain.artboard.ArtImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtImgRepository extends JpaRepository<ArtImg, Long> {
+}
---
+++ src/main/java/com/nailshop/nailborhood/repository/artboard/ArtRefRepository.java
@@ -0,0 +1,7 @@
+package com.nailshop.nailborhood.repository.artboard;
+
+import com.nailshop.nailborhood.domain.artboard.ArtRef;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtRefRepository extends JpaRepository<ArtRef, Long> {
+}
