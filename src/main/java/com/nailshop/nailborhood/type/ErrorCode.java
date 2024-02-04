406ed982612834d4588f96016c62f62aaf594834
wnsdy <wnsdyd1012@gmail.com>
2024-02-04 16:56:35 +0900

    :art: [#17] add new messages

--- src/main/java/com/nailshop/nailborhood/type/ErrorCode.java
+++ src/main/java/com/nailshop/nailborhood/type/ErrorCode.java
@@ -12,9 +12,9 @@ public enum ErrorCode {
     FILE_EXTENSION_NOT_FOUND("이미지를 찾을 수 없습니다"),
     // 매장
     SHOP_NOT_FOUND("해당 매장 정보를 찾을 수 없습니다"),
-    SHOPSTAUTS_NOT_FOUND("매장 상태 정보를 찾을 수 없습니다");
-
-
+    SHOPSTAUTS_NOT_FOUND("매장 상태 정보를 찾을 수 없습니다"),
+    // 카테고리
+    CATEGORY_NOT_FOUND("카테고리를 찾을 수 없습니다.");
 
     private final String description;
 }
--- src/main/java/com/nailshop/nailborhood/type/SuccessCode.java
+++ src/main/java/com/nailshop/nailborhood/type/SuccessCode.java
@@ -9,8 +9,11 @@ import lombok.Getter;
 public enum SuccessCode {
 
     EXAMPLE_SUCCESS("성공 예시 코드 입니다."),
+    // 매장
     SHOP_REGISTRATION_SUCCESS("매장 등록을 성공했습니다."),
-    SHOP_MODIFICATION_SUCCESS("매장 수정을 성공했습니다.");
+    SHOP_MODIFICATION_SUCCESS("매장 수정을 성공했습니다."),
+    // 아트판
+    ART_REGISTRATION_SUCCESS("아트판 등록에 성공했습니다.");
 
 
     private final String description;
