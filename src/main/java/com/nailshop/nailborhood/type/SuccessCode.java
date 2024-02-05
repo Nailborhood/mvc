5fef2c3e08adc44bec39873ac31a6a92e3451e06
eumssooo <mamma1115@gmail.com>
2024-02-05 14:49:11 +0900

    :sparkles: Implemented member email availability check function

--- src/main/java/com/nailshop/nailborhood/type/SuccessCode.java
+++ src/main/java/com/nailshop/nailborhood/type/SuccessCode.java
@@ -7,7 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    EXAMPLE_SUCCESS("성공 예시 코드 입니다.");
+    EXAMPLE_SUCCESS("성공 예시 코드 입니다."),
+
+
+
+    // 멤버
+    EMAIL_AVAILABLE("사용 가능한 이메일입니다"),
+    EMAIL_NOT_AVAILABLE("중복된 이메일입니다");
 
     private final String description;
 }
