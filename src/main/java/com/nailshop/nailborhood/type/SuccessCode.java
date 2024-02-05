eb9b483c203227877f20cd000c6ce241c83b0f0e
eumssooo <mamma1115@gmail.com>
2024-02-05 14:49:11 +0900

    :sparkles: Implemented member email availability check function

--- src/main/java/com/nailshop/nailborhood/type/SuccessCode.java
+++ src/main/java/com/nailshop/nailborhood/type/SuccessCode.java
@@ -1,6 +1,5 @@
 package com.nailshop.nailborhood.type;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,9 +12,12 @@ public enum SuccessCode {
     SHOP_MODIFICATION_SUCCESS("매장 수정을 성공했습니다."),
     ALL_SHOP_LOOKUP_SUCCESS("매장 조회를 성공했습니다"),
     SHOP_DETAIL_LOOKUP_SUCCESS("매장 상세 조회를 성공했습니다"),
-    REVIEW_REGISTRATION_SUCCESS("리뷰 등록을 성공했습니다");
+    REVIEW_REGISTRATION_SUCCESS("리뷰 등록을 성공했습니다"),
 
 
+    // 멤버
+    EMAIL_AVAILABLE("사용 가능한 이메일입니다"),
+    EMAIL_NOT_AVAILABLE("중복된 이메일입니다");
 
     private final String description;
 }
