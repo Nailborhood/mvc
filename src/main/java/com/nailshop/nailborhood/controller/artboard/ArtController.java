5647fb6a2610fd828731eccec5d26199757505e4
wnsdy <wnsdyd1012@gmail.com>
2024-02-06 16:42:22 +0900

    :sparkles: [#16] develop all Artboard inquiry

--- src/main/java/com/nailshop/nailborhood/controller/artboard/ArtController.java
+++ src/main/java/com/nailshop/nailborhood/controller/artboard/ArtController.java
@@ -1,14 +1,9 @@
 package com.nailshop.nailborhood.controller.artboard;
 
-import com.nailshop.nailborhood.dto.artboard.ArtLikeDto;
-import com.nailshop.nailborhood.dto.artboard.ArtRegistrationDto;
-import com.nailshop.nailborhood.dto.artboard.ArtUpdateDto;
+import com.nailshop.nailborhood.dto.artboard.*;
 import com.nailshop.nailborhood.dto.common.CommonResponseDto;
 import com.nailshop.nailborhood.dto.common.ResultDto;
-import com.nailshop.nailborhood.service.artboard.ArtDeleteService;
-import com.nailshop.nailborhood.service.artboard.ArtLikeService;
-import com.nailshop.nailborhood.service.artboard.ArtRegistrationService;
-import com.nailshop.nailborhood.service.artboard.ArtUpdateService;
+import com.nailshop.nailborhood.service.artboard.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -27,13 +22,14 @@ public class ArtController {
     private final ArtUpdateService artUpdateService;
     private final ArtDeleteService artDeleteService;
     private final ArtLikeService artLikeService;
+    private final ArtInquiryService artInquiryService;
 
     @Tag(name = "owner", description = "owner API")
     @Operation(summary = "아트판 등록", description = "owner API")
     @PostMapping(consumes = {"multipart/form-data"}, value = "/owner/artboard/register")
     public ResponseEntity<ResultDto<Void>> registerArt(@RequestPart(value = "file") List<MultipartFile> multipartFileList,
-                                                       @RequestPart(value = "data") ArtRegistrationDto artRegistrationDto){
-        CommonResponseDto<Object> registerArt = artRegistrationService.registerArt(multipartFileList, artRegistrationDto);
+                                                       @RequestPart(value = "data") ArtRegistrationRequestDto artRegistrationRequestDto){
+        CommonResponseDto<Object> registerArt = artRegistrationService.registerArt(multipartFileList, artRegistrationRequestDto);
         ResultDto<Void> resultDto = ResultDto.in(registerArt.getStatus(), registerArt.getMessage());
 
         return ResponseEntity.status(registerArt.getHttpStatus()).body(resultDto);
@@ -44,8 +40,8 @@ public class ArtController {
     @PutMapping(consumes = {"multipart/form-data"}, value = "/owner/artboard/modify/{artRefId}")
     public ResponseEntity<ResultDto<Void>> updateArtRef(@PathVariable Long artRefId,
                                                         @RequestPart(value = "file") List<MultipartFile> multipartFileList,
-                                                        @RequestPart(value = "data") ArtUpdateDto artUpdateDto){
-        CommonResponseDto<Object> updateArt = artUpdateService.updateArt(multipartFileList, artUpdateDto, artRefId);
+                                                        @RequestPart(value = "data") ArtUpdateRequestDto artUpdateRequestDto){
+        CommonResponseDto<Object> updateArt = artUpdateService.updateArt(multipartFileList, artUpdateRequestDto, artRefId);
         ResultDto<Void> resultDto = ResultDto.in(updateArt.getStatus(), updateArt.getMessage());
 
         return ResponseEntity.status(updateArt.getHttpStatus()).body(resultDto);
@@ -65,11 +61,24 @@ public class ArtController {
     @Tag(name = "user", description = "user API")
     @Operation(summary = "아트판 좋아요", description = "user API")
     @PostMapping("/user/artboard/like/{artRefId}")
-    public ResponseEntity<ResultDto<ArtLikeDto>> likeArtRef(@PathVariable Long artRefId){
+    public ResponseEntity<ResultDto<ArtLikeResponseDto>> likeArtRef(@PathVariable Long artRefId){
         CommonResponseDto<Object> likeArt = artLikeService.likeArt(artRefId);
-        ResultDto<ArtLikeDto> resultDto = ResultDto.in(likeArt.getStatus(), likeArt.getMessage());
-        resultDto.setData((ArtLikeDto) likeArt.getData());
+        ResultDto<ArtLikeResponseDto> resultDto = ResultDto.in(likeArt.getStatus(), likeArt.getMessage());
+        resultDto.setData((ArtLikeResponseDto) likeArt.getData());
 
         return ResponseEntity.status(likeArt.getHttpStatus()).body(resultDto);
+    }
+
+    @Tag(name = "user", description = "user API")
+    @Operation(summary = "아트판 전체 조회", description = "user API")
+    @GetMapping("/user/artboard/inquiry")
+    public ResponseEntity<ResultDto<ArtListResponseDto>> inquiryAllArtRef(@RequestParam(value = "page", defaultValue = "1", required = false) int page,
+                                                                      @RequestParam(value = "size", defaultValue = "10", required = false) int size,
+                                                                      @RequestParam(value = "sortBy", defaultValue = "updatedAt", required = false) String sortBy){
+        CommonResponseDto<Object> inquiryAllArt = artInquiryService.inquiryAllArt(page, size, sortBy);
+        ResultDto<ArtListResponseDto> resultDto = ResultDto.in(inquiryAllArt.getStatus(), inquiryAllArt.getMessage());
+        resultDto.setData((ArtListResponseDto) inquiryAllArt.getData());
+
+        return ResponseEntity.status(inquiryAllArt.getHttpStatus()).body(resultDto);
     }
 }
