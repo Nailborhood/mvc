62454eb5885459578677ba36e7651e2b739c3f35
dannab <koogoori54@gmail.com>
2024-02-09 22:04:00 +0900

    :recycle: [#7] Change review report status

--- src/main/java/com/nailshop/nailborhood/type/ReviewReportStatus.java
+++ src/main/java/com/nailshop/nailborhood/type/ReviewReportStatus.java
@@ -8,9 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ReviewReportStatus {
 
     REVIEW_REPORT_PENDING("신고 대기중"),
-    REVIEW_REPORT_IN_PROCESS("신고 처리중"),
     REVIEW_REPORT_REJECTED("신고 반려됨"),
-    REVIEW_REPORT_ACCEPTED("신고 접수됨");
+    REVIEW_REPORT_ACCEPTED("신고 처리됨");
 
     private final String description;
 }
