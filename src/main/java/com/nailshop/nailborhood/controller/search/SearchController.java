32cd03ec5d445d399bab758205a3c515737c27bf
dannab <koogoori54@gmail.com>
2024-02-14 04:38:37 +0900

    :art: Apply Swagger for API Documentation for Review, Mypage, Search

--- src/main/java/com/nailshop/nailborhood/controller/search/SearchController.java
+++ src/main/java/com/nailshop/nailborhood/controller/search/SearchController.java
@@ -6,6 +6,8 @@ import com.nailshop.nailborhood.dto.common.ResultDto;
 import com.nailshop.nailborhood.dto.example.ExampleDto;
 import com.nailshop.nailborhood.dto.review.response.ReviewListResponseDto;
 import com.nailshop.nailborhood.service.search.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,8 @@ public class SearchController {
     private final SearchService searchService;
 
     // 리뷰 검색
+    @Tag(name = "search", description = "search API")
+    @Operation(summary = "리뷰 검색", description = "search API")
     @GetMapping("/search/review")
     public ResponseEntity<ResultDto<ReviewListResponseDto>> searchReviewInquiry(@RequestParam(value = "keyword") String keyword,
                                                                                 @RequestParam(value = "page", defaultValue = "1", required = false) int page,
@@ -31,6 +35,8 @@ public class SearchController {
     }
 
     // 아트판 검색
+    @Tag(name = "search", description = "search API")
+    @Operation(summary = "아트 검색", description = "search API")
     @GetMapping("/search/artRef")
     public ResponseEntity<ResultDto<ArtListResponseDto>> searchArtRefInquiry(@RequestParam(value = "keyword") String keyword,
                                                                              @RequestParam(value = "page", defaultValue = "1", required = false) int page,
