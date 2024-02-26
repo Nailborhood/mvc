78aaedfef09e95b0a0689c05e5ec8d7be06ed344
eumssooo <mamma1115@gmail.com>
2024-02-26 18:34:41 +0900

    :lock: add httpBasic setting

--- src/main/java/com/nailshop/nailborhood/security/config/SecurityConfig.java
+++ src/main/java/com/nailshop/nailborhood/security/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -63,6 +64,7 @@ public class SecurityConfig {
                                 .deleteCookies("refreshToken")
                                 .invalidateHttpSession(true)
                 )
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf((csrf) ->
                         csrf
                                 .disable()
