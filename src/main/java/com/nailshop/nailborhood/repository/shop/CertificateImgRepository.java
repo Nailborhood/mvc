c5d310f4fe59853f717343220450b55adc3fc553
younssue <love_rvd7@naver.com>
2024-02-29 19:50:58 +0900

    :art: ERD Modification: Add certificateImg [#80]

---
+++ src/main/java/com/nailshop/nailborhood/repository/shop/CertificateImgRepository.java
@@ -0,0 +1,11 @@
+package com.nailshop.nailborhood.repository.shop;
+
+import com.nailshop.nailborhood.domain.shop.CertificateImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CertificateImgRepository extends JpaRepository<CertificateImg,Long> {
+
+
+
+}
