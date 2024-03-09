eef88344edfb511913c6ced1f6d12ededdaef98f
eumssooo <mamma1115@gmail.com>
2024-03-09 21:32:54 +0900

    :lipstick: add sidebar css

--- src/main/java/com/nailshop/nailborhood/controller/mvcExample/ExampleController.java
+++ src/main/java/com/nailshop/nailborhood/controller/mvcExample/ExampleController.java
@@ -12,8 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ExampleController {
 
     @GetMapping("/example")
-    public String hello(Model model) {
+    public String exampleHello(Model model) {
         model.addAttribute("message", "안녕");
         return "example/hello"; //html 이름
+    }
+    @GetMapping("/example/mypage")
+    public String exampleMypage(Model model) {
+        return "example/mypage_example"; //html 이름
+    }
+
+    @GetMapping("/example/owner")
+    public String exampleOwner(Model model) {
+        return "example/owner_example"; //html 이름
+    }
+
+    @GetMapping("/example/admin")
+    public String exampleAdmin(Model model) {
+        return "example/admin_example"; //html 이름
     }
 }
