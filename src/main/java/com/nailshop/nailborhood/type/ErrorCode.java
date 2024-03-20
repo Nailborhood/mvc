d32939aebfee386f30d51886be14978f4a40988d
younssue <love_rvd7@naver.com>
2024-03-20 11:27:43 +0900

    :art: get address data in DB [#10] [#10]

--- src/main/java/com/nailshop/nailborhood/type/ErrorCode.java
+++ src/main/java/com/nailshop/nailborhood/type/ErrorCode.java
@@ -7,8 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    // 멤버
+    // customer,owner
     CUSTOMER_NOT_FOUND("고객을 찾을 수 없습니다."),
+    OWNER_ALREADY_EXIST("이미 매장 신청을 하여, 더이상 매장 신청을 할 수 없습니다"),
 
     // 이미지 업로드
     IMAGE_UPLOAD_FAIL("이미지 업로드 실패"),
@@ -21,6 +22,7 @@ public enum ErrorCode {
     SHOP_NOT_FOUND("매장 정보를 찾을 수 없습니다"),
     SHOPSTATUS_NOT_FOUND("매장 상태 정보를 찾을 수 없습니다"),
     SHOP_REQUEST_NOT_FOUND("매장 신청 정보를 찾을 수 없습니다"),
+    SHOP_REGISTRATION_FAIL("매장 등록 신청에 실패 했습니다"),
 
     // Member
     MEMBER_NOT_FOUND("유저 정보 조회에 실패하였습니다."),
