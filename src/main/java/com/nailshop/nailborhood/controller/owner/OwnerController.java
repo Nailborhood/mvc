b9cae11410987f063f0c2f03d769e6b6dd87af39
younssue <love_rvd7@naver.com>
2024-03-20 16:00:49 +0900

    :package: change package structure and file name

--- src/main/java/com/nailshop/nailborhood/controller/owner/OwnerController.java
+++ src/main/java/com/nailshop/nailborhood/controller/owner/OwnerController.java
@@ -1,7 +1,12 @@
 package com.nailshop.nailborhood.controller.owner;
 
+import com.nailshop.nailborhood.dto.artboard.ArtListResponseDto;
 import com.nailshop.nailborhood.dto.common.CommonResponseDto;
+import com.nailshop.nailborhood.dto.common.ResultDto;
+import com.nailshop.nailborhood.service.artboard.ArtInquiryService;
 import com.nailshop.nailborhood.service.owner.OwnerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/nailborhood")
 public class OwnerController {
 
     private final OwnerService ownerService;
+    private final ArtInquiryService artInquiryService;
 
     // 검색기능이랑 통합
     @GetMapping("/owner/review/{shopId}")
@@ -30,5 +35,25 @@ public class OwnerController {
         model.addAttribute("reviewList", shopReview.getData());
 
         return "owner/review_manage";
+    }
+
+    @Tag(name = "owner", description = "owner API")
+    @Operation(summary = "아트판 관리", description = "owner API")
+    @GetMapping("/owner/artboard/manage")
+    public String inquiryAllArtRef(/*@RequestHeader(AUTH) String accessToken,*/
+            @RequestParam(value = "page", defaultValue = "1", required = false) int page,
+            @RequestParam(value = "size", defaultValue = "5", required = false) int size,
+            @RequestParam(value = "sortBy", defaultValue = "updatedAt", required = false) String sortBy,
+            @RequestParam(value = "category", defaultValue = "", required = false) String category,
+            Model model){
+        CommonResponseDto<Object> inquiryAllArt = artInquiryService.inquiryAllArtByShopId(/*accessToken, */page, size, sortBy, category);
+        ResultDto<ArtListResponseDto> resultDto = ResultDto.in(inquiryAllArt.getStatus(), inquiryAllArt.getMessage());
+        resultDto.setData((ArtListResponseDto) inquiryAllArt.getData());
+
+        model.addAttribute("result", resultDto);
+        model.addAttribute("sortBy", sortBy);
+        model.addAttribute("size", size);
+
+        return "artboard/art_manage";
     }
 }
