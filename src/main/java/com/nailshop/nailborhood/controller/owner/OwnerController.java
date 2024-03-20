97dfa3813a80cceb56f2c50e1e180ae757bffad0
younssue <love_rvd7@naver.com>
2024-03-20 21:06:01 +0900

    :art:Implement store modifications [#16]

--- src/main/java/com/nailshop/nailborhood/controller/owner/OwnerController.java
+++ src/main/java/com/nailshop/nailborhood/controller/owner/OwnerController.java
@@ -3,17 +3,29 @@ package com.nailshop.nailborhood.controller.owner;
 import com.nailshop.nailborhood.dto.artboard.ArtListResponseDto;
 import com.nailshop.nailborhood.dto.common.CommonResponseDto;
 import com.nailshop.nailborhood.dto.common.ResultDto;
+import com.nailshop.nailborhood.dto.review.response.ReviewDetailResponseDto;
+import com.nailshop.nailborhood.dto.shop.request.ShopModifiactionRequestDto;
+import com.nailshop.nailborhood.dto.shop.response.StoreAddressSeparationListDto;
+import com.nailshop.nailborhood.exception.NotFoundException;
 import com.nailshop.nailborhood.service.artboard.ArtInquiryService;
 import com.nailshop.nailborhood.service.owner.OwnerService;
+import com.nailshop.nailborhood.service.shop.ShopDetailService;
+import com.nailshop.nailborhood.service.shop.owner.ShopModificationService;
+import com.nailshop.nailborhood.service.shop.owner.ShopRegistrationService;
+import com.nailshop.nailborhood.type.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+import static com.nailshop.nailborhood.security.service.jwt.TokenProvider.AUTH;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,16 +33,19 @@ public class OwnerController {
 
     private final OwnerService ownerService;
     private final ArtInquiryService artInquiryService;
+    private final ShopModificationService shopModificationService;
+    private final ShopDetailService shopDetailService;
+    private final ShopRegistrationService shopRegistrationService;
 
     // 검색기능이랑 통합
     @GetMapping("/owner/review/{shopId}")
     public String getShopReviewList(Model model,
                                     @PathVariable Long shopId,
-                                    @RequestParam(value = "keyword",required = false) String keyword,
+                                    @RequestParam(value = "keyword", required = false) String keyword,
                                     @RequestParam(value = "page", defaultValue = "1", required = false) int page,
                                     @RequestParam(value = "size", defaultValue = "10", required = false) int size,
                                     @RequestParam(value = "orderby", defaultValue = "createdAt", required = false) String criteria,
-                                    @RequestParam(value = "sort", defaultValue = "DESC", required = false) String sort){
+                                    @RequestParam(value = "sort", defaultValue = "DESC", required = false) String sort) {
         CommonResponseDto<Object> shopReview = ownerService.getAllReviewListByShopId(keyword, page, size, criteria, sort, shopId);
         model.addAttribute("reviewList", shopReview.getData());
 
@@ -45,7 +60,7 @@ public class OwnerController {
             @RequestParam(value = "size", defaultValue = "5", required = false) int size,
             @RequestParam(value = "sortBy", defaultValue = "updatedAt", required = false) String sortBy,
             @RequestParam(value = "category", defaultValue = "", required = false) String category,
-            Model model){
+            Model model) {
         CommonResponseDto<Object> inquiryAllArt = artInquiryService.inquiryAllArtByShopId(/*accessToken, */page, size, sortBy, category);
         ResultDto<ArtListResponseDto> resultDto = ResultDto.in(inquiryAllArt.getStatus(), inquiryAllArt.getMessage());
         resultDto.setData((ArtListResponseDto) inquiryAllArt.getData());
@@ -55,5 +70,45 @@ public class OwnerController {
         model.addAttribute("size", size);
 
         return "artboard/art_manage";
+    }
+
+
+    // TODO: user 연결 필요
+    // 매장 정보 수정
+
+    @GetMapping("/owner/update/{shopId}")
+    public String updateShop(Model model,
+                             @PathVariable Long shopId) {
+        CommonResponseDto<Object> shopDetail = shopDetailService.getShopDetail(shopId);
+        ResultDto<ReviewDetailResponseDto> resultDto = ResultDto.in(shopDetail.getStatus(), shopDetail.getMessage());
+        resultDto.setData((ReviewDetailResponseDto) shopDetail.getData());
+
+        StoreAddressSeparationListDto storeAddressSeparationListDtoList = shopRegistrationService.findAddress();
+
+        model.addAttribute("shopDto", resultDto);
+        model.addAttribute("addressDto", storeAddressSeparationListDtoList);
+        return "owner/owner_shop_update";
+    }
+
+    @PostMapping("/owner/update/{shopId}")
+    public String updateShop(Model model,
+                             @PathVariable Long shopId,
+                             @RequestPart(value = "file") List<MultipartFile> multipartFileList,
+                             @ModelAttribute ShopModifiactionRequestDto shopModifiactionRequestDto,
+                             RedirectAttributes redirectAttributes) {
+        try {
+            CommonResponseDto<Object> commonResponseDto = shopModificationService.updateShop(shopId, multipartFileList, shopModifiactionRequestDto);
+            ResultDto<Void> resultDto = ResultDto.in(commonResponseDto.getStatus(), commonResponseDto.getMessage());
+            redirectAttributes.addFlashAttribute("successMessage", resultDto.getMessage());
+            redirectAttributes.addAttribute("shopId", shopId);
+            // 매장 상세 페이지로 이동
+            return "redirect:/shopDetail/{shopId}";
+        } catch (NotFoundException e) {
+            redirectAttributes.addFlashAttribute("errorCode", ErrorCode.SHOP_UPDATE_FAIL);
+            // 매장 수정 페이지로 이동
+            return "owner/owner_shop_update";
+        }
+
+
     }
 }
