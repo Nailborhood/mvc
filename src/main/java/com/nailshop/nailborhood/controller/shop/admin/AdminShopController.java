9c7b248f5f86cd53ef25567c66d96d75b699557d
wnsdy <wnsdyd1012@gmail.com>
2024-03-27 17:45:30 +0900

    :sparkles: add shopRegistration approval/rejection

--- src/main/java/com/nailshop/nailborhood/controller/shop/admin/AdminShopController.java
+++ src/main/java/com/nailshop/nailborhood/controller/shop/admin/AdminShopController.java
@@ -2,12 +2,12 @@ package com.nailshop.nailborhood.controller.shop.admin;
 
 import com.nailshop.nailborhood.dto.common.CommonResponseDto;
 import com.nailshop.nailborhood.dto.common.ResultDto;
-import com.nailshop.nailborhood.dto.shop.request.ShopModifiactionRequestDto;
 import com.nailshop.nailborhood.exception.NotFoundException;
+import com.nailshop.nailborhood.service.member.admin.ShopRegistrationHandler;
+import com.nailshop.nailborhood.service.shop.ShopDetailService;
 import com.nailshop.nailborhood.service.shop.admin.ShopDeleteService;
 import com.nailshop.nailborhood.service.shop.admin.ShopRequestLookupAdminService;
 import com.nailshop.nailborhood.service.shop.admin.ShopStatusChangeService;
-import com.nailshop.nailborhood.service.shop.owner.ShopModificationService;
 import com.nailshop.nailborhood.type.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,9 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 import static com.nailshop.nailborhood.security.service.jwt.TokenProvider.AUTH;
 
@@ -30,7 +27,8 @@ public class AdminShopController {
     private final ShopDeleteService shopDeleteService;
     private final ShopStatusChangeService shopStatusChangeService;
     private final ShopRequestLookupAdminService shopRequestLookupAdminService;
-
+    private final ShopRegistrationHandler shopRegistrationHandler;
+    private final ShopDetailService shopDetailService;
 
 
 
@@ -58,6 +56,17 @@ public class AdminShopController {
         }
     }
 
+    // 매장 신청 상세 조회
+    @GetMapping("/shopRegistrationDetail/{shopId}")
+    public String getShopDetail(Model model,
+                                @PathVariable Long shopId){
+        CommonResponseDto<Object> shopDetail = shopDetailService.getShopDetail(shopId);
+
+        model.addAttribute("shopDetail", shopDetail.getData());
+
+        return "admin/admin_shop_registration";
+    }
+
     @Tag(name = "admin", description = "admin API")
     @Operation(summary = "매장 삭제", description = "admin API")
     // 매장 삭제
@@ -82,5 +91,27 @@ public class AdminShopController {
 
         return ResponseEntity.status(commonResponseDto.getHttpStatus())
                              .body(resultDto);
+    }
+
+    @Tag(name = "admin", description = "admin API")
+    @Operation(summary = "매장등록신청 승인", description = "admin API")
+    @PutMapping("/admin/shop/approve/{shopId}")
+    public ResponseEntity<ResultDto<Void>> shopApprove(/*@RequestHeader(AUTH) String accessToken,*/
+                                                       @PathVariable Long shopId){
+        CommonResponseDto<Object> shopApprove = shopRegistrationHandler.shopApprove(/*accessToken, */shopId);
+        ResultDto<Void> resultDto = ResultDto.in(shopApprove.getStatus(), shopApprove.getMessage());
+
+        return ResponseEntity.status(shopApprove.getHttpStatus()).body(resultDto);
+    }
+
+    @Tag(name = "admin", description = "admin API")
+    @Operation(summary = "매장등록신청 거절", description = "admin API")
+    @DeleteMapping("/admin/shop/reject/{shopId}")
+    public ResponseEntity<ResultDto<Void>> shopReject(/*@RequestHeader(AUTH) String accessToken,*/
+                                                      @PathVariable Long shopId){
+        CommonResponseDto<Object> shopReject = shopRegistrationHandler.shopReject(/*accessToken, */shopId);
+        ResultDto<Void> resultDto = ResultDto.in(shopReject.getStatus(), shopReject.getMessage());
+
+        return ResponseEntity.status(shopReject.getHttpStatus()).body(resultDto);
     }
 }
