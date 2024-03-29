814b0d4e415148ab02f6bc1694a4ec346414f4a6
dannab <koogoori54@gmail.com>
2024-03-29 16:35:48 +0900

    :art: [#20] add reviewReport View

---
+++ src/main/resources/static/js/review/review_report.js
@@ -0,0 +1,22 @@
+function reviewReport(button) {
+    const reviewId = button.getAttribute('data-id');
+    const shopId = button.getAttribute('data-shop-id'); // shopId 값을 얻음
+    const reportReason = document.getElementById('report-reason').value;
+    if (confirm("정말로 신고하시겠습니까?")) {
+        fetch(`/review/report/${reviewId}?shopId=${shopId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ reportReason }),
+        }).then(response => response.json())
+            .then(data => {
+                alert(data.message);
+                if (data.status === 'success') {
+                    // 성공했다면 페이지를 새로고침하거나 사용자를 다른 페이지로 리다이렉션
+                    location.reload();
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    }
+}
