042c3b3c0a7b739dbae54ba034bfdff67def6369
eumssooo <mamma1115@gmail.com>
2024-04-01 20:17:50 +0900

    :art: update functions

--- src/main/java/com/nailshop/nailborhood/controller/mvcExample/ExampleController.java
+++ src/main/java/com/nailshop/nailborhood/controller/mvcExample/ExampleController.java
@@ -1,26 +1,30 @@
 package com.nailshop.nailborhood.controller.mvcExample;
 
 import com.nailshop.nailborhood.security.config.auth.MemberDetails;
+import com.nailshop.nailborhood.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequiredArgsConstructor
 public class ExampleController {
 
+    private final MemberService memberService;
+
     @GetMapping("/example")
     public String hello(@AuthenticationPrincipal MemberDetails memberDetails, Model model) {
-        String nicknameSpace;
-        if(memberDetails == null) {
-            nicknameSpace = "";
-        } else {
-            nicknameSpace = memberDetails.getNickname() + "님";
-        }
+        String nicknameSpace = (memberDetails != null) ? memberDetails.getMember().getNickname() : "";
         model.addAttribute("memberNickname", nicknameSpace);
+
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
         model.addAttribute("message", "안녕");
         return "example/hello"; //html 이름
     }
--- src/main/resources/static/js/member/signup.js
+++ src/main/resources/static/js/member/signup.js
@@ -1,19 +1,3 @@
-// const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+$/;
-//
-// function checkEmailForm() {
-//     let emailInput = document.getElementById("email-input").value;
-//     let emailMessage = document.getElementById("email-mismatch_message");
-//     console.log(emailInput)
-//     if(emailValidChk(emailInput)) {
-//         emailMessage.style.display = "none";
-//     } else {
-//         emailMessage.style.display = "table-row";
-//     }
-// }
-// function emailValidChk(email) {
-//     return pattern.test(email) !== false;
-// }
-
 
 const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+$/;
 
@@ -29,8 +13,31 @@ function checkEmailForm() {
         emailMessage.style.display = "table-row";
     }
 }
+
 function emailValidChk(email) {
     return pattern.test(email) !== false;
+}
+
+function checkEmailDuplicated() {
+    let emailInput = document.getElementById("email-input").value;
+    $.ajax({
+        url : '/checkEmail',
+        type : 'GET',
+        dataType:"text",
+        data : {"email" : emailInput},
+        success : function (exist) {
+            if(exist) {
+                $("#email-unavailable-message").attr('style','display:inline')
+                $("#email-available-message").attr('style','display:none')
+            } else {
+                $("#email-unavailable-message").attr('style','display:none')
+                $("#email-available-message").attr('style','display:inline')
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("Error: " + status + " - " + error);
+        }
+    })
 }
 
 window.onload = function () {
@@ -42,14 +49,64 @@ window.onload = function () {
     });
 
     passwordInput.addEventListener("input", function () {
-        console.log(passwordInput.value);
-        if (checkPasswordForm(passwordInput.value)) {
+        var password = passwordInput.value;
+        if (checkPasswordForm(password)) {
             patternMessage_tr.style.display = "none";
         } else {
             patternMessage_tr.style.display = "table-row";
         }
     });
+
+    // 닉네임 중복 체크
+    var nicknameInput = document.querySelector("#nickname-input");
+    nicknameInput.addEventListener("input", debounce(nicknameCheck(nicknameInput), 150)); // 150ms 디바운스 지연
+
+    // 전화번호 숫자만 입력 (작업중)
+    var phoneNumberInput = document.getElementById("phonenumber-input");
+    phoneNumberInput.addEventListener("keydown", onKeyPressHandler);
 };
+
+// debounce 함수 정의
+function debounce(func, delay) {
+    let timer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            func.apply(context, args);
+        }, delay);
+    };
+}
+
+function nicknameCheck(input) {
+    const nickname = input.value.trim();
+    console.log(nickname);
+    if (nickname === "") { // 입력값 비어있음
+        $("#nickname-unavailable-message").attr('style', 'display:none');
+        $("#nickname-available-message").attr('style', 'display:none');
+        return;
+    }
+    $.ajax({
+        url: '/checkNickname',
+        type: 'GET',
+        dataType: "text",
+        data: {"nickname": nickname},
+        success: function (exist) {
+            console.log(exist);
+            if(exist === 'true') {
+                $("#nickname-available_message").attr('style', 'display:none');
+                $("#nickname-unavailable_message").attr('style', 'display:table-row');
+            } else {
+                $("#nickname-unavailable_message").attr('style', 'display:none');
+                $("#nickname-available_message").attr('style', 'display:table-row');
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("Error: " + status + " - " + error);
+        }
+    });
+}
 
 function checkPasswordForm(input) {
     let reg = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/;
@@ -58,11 +115,10 @@ function checkPasswordForm(input) {
 
 function passwordCheckMatch() {
     var passwordInput = document.querySelector("#password-input").value;
-    var passwordCheckInput = document.querySelector(
-        "#password-check-input"
-    ).value;
+    var passwordCheckInput = document.querySelector("#password-check-input").value;
     var message_div = document.querySelector("#password-match_div");
     let message_text = document.querySelector("#password-match_message");
+
     message_div.style.display = "table-row";
     if (passwordInput != passwordCheckInput) {
         message_text.innerHTML = "<div>비밀번호와 일치하지 않습니다.</div>";
@@ -70,3 +126,28 @@ function passwordCheckMatch() {
         message_div.style.display = "none";
     }
 }
+
+// 전화번호 숫자만 입력
+
+function isNumericInput(event) {
+    var key = event.keyCode;
+    return (key >= 48 && key <= 57) || // 0-9 키
+        (key >= 96 && key <= 105) || // numpad 0-9 키
+        key === 8 || key === 9 || key === 37 || key === 39 || // 백스페이스, 탭, 왼쪽 화살표, 오른쪽 화살표 키
+        (event.ctrlKey && key === 65) || // Ctrl+A
+        (event.ctrlKey && key === 67) || // Ctrl+C
+        (event.ctrlKey && key === 88) || // Ctrl+X
+        (event.ctrlKey && key === 86) || // Ctrl+V
+        (event.ctrlKey && key === 90) || // Ctrl+Z
+        (event.ctrlKey && key === 89); // Ctrl+Y
+}
+
+function onKeyPressHandler(event) {
+    if (!isNumericInput(event)) {
+        event.preventDefault();
+    }
+}
+
+
+
+
