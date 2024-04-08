7dc815dc78000eb863905e8118cec16197fea909
eumssooo <mamma1115@gmail.com>
2024-04-08 17:38:56 +0900

    :sparkles: add password update function

--- src/main/java/com/nailshop/nailborhood/dto/member/request/ModPasswordRequestDto.java
+++ src/main/java/com/nailshop/nailborhood/dto/member/request/ModPasswordRequestDto.java
@@ -2,11 +2,14 @@ package com.nailshop.nailborhood.dto.member.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class ModPasswordRequestDto {
-    private String password;
-    private String passwordCheck;
+    private String oldPassword;
+    private String newPassword;
+    private String newPasswordCheck;
 
 }
