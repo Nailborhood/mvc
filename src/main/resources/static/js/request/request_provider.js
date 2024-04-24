fe3f1a71bb13217bcf4893b4d350ac71c4f2ba6e
younssue <love_rvd7@naver.com>
2024-04-24 22:54:17 +0900

    :art: Modification of the function of not being able to apply for change to the mode of owner excluding provider Nail login user

--- src/main/resources/static/js/request/request_provider.js
+++ src/main/resources/static/js/request/request_provider.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         } else if (provider === 'google') {
             // 확인 대화 상자를 표시
-            if (confirm('Google 계정으로는 사업자 신청을 할 수 없습니다. 내동네일 로그인 페이지로 이동하시겠습니까?')) {
+            if (confirm('Google 계정으로는 사업자 신청을 할 수 없습니다. 내동네일 회원가입 페이지로 이동하시겠습니까?')) {
                 // window.location.href = '/login';
                 logoutAndRedirect();
             }
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(response => {
                 if (response.ok) {
                     console.log('로그아웃 성공');
-                    window.location.href = '/login';  // 로그아웃 후 로그인 페이지로 이동
+                    window.location.href = '/signup';  // 로그아웃 후 회원가입 페이지로 이동
                 } else {
                     throw new Error('로그아웃 실패');
                 }
